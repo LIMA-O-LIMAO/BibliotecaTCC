4425de01793cda0ae84f6573319d76a3667286dd
Lima <joaovitorlima0411@gmail.com>
2023-09-08 00:20:05 -0300

    Get-livro/Post-aluno

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,51 @@
+'use client'
 
+import Link from 'next/link'
+
+ 
+import { useState } from "react"
+import axios from 'axios';
+import { text } from 'stream/consumers';
 
 export default function Home() {
+
+
+
+const [setNome,Nome]=useState("");
+const [setSenha,Senha]=useState("");
+
+
+
+
+
+  const Clique=()=>{
+
+
+
+
+
+
+
+//if(Nome=variavelBd && Senha=VariavelsBD){
+
+
+
+//return(
+
+  //<Link href="./pages/"/>
+//)
+
+//}
+
+//senha=variaveldoBD
+//nome tbm, logue
+
+
+
+
+
+
+  }
   
   return (
 <main>
@@ -11,14 +56,14 @@ export default function Home() {
 <div className='bg-zinc-800/90 flex justify-center flex-col w-80 h-[450px] items-center rounded-2xl gap-12'>
 <h1 className='text-3xl text-white '>Login</h1>
 <div className='mt-2 flex items-center justify-center flex-col gap-5 '>
-<input  type="text" placeholder='joao' />
+<input  type="text" value='Nome_aluno' placeholder='joao' />
 
 
-<input type="password" placeholder='***' />
+<input type='text' placeholder='***' />
 </div>
-<a href="./pages/">
-<button  className='rounded-[0.8rem] bg-white h-10 w-48'>Logar</button>
-</a>
+<Link href="./pages/">
+<button onClick={Clique}  className='rounded-[0.8rem] bg-white h-10 w-48'>Logar</button>
+</Link>
 </div>
 
 </div>
--- app/pages/formularios/page.tsx
+++ app/pages/formularios/page.tsx
@@ -1,53 +1,87 @@
-'use client'
-import { useState,useEffect } from "react"
-import Nav from "../../componentes/nav/navb"
-import axios from "axios"
-export default function Formulario() {
+  'use client'
+  import { useState,useEffect } from "react"
+  import Nav from "../../componentes/nav/navb"
+  import axios from "axios"
+  export default function Formulario() {
 
-    const [postss, setPostss] = useState([]);
+      const [postss, setPostss] = useState([]);
 
 
-    useEffect(()=>{
+      //useEffect(()=>{
 
-axios.get('http://localhost/php/api.php').then(function(res){
+  //axios.get('http://localhost/php/api.php').then(function(res){
 
-setPostss(res.data)
-}
+  //setPostss(res.data)
+  //}
 
-)
-    },[])
-    
-    return (
-        <main className="h-screen w-screen">
-
-        <Nav/>
-
-  
-<section className=" h-[91.5vh] w-full pt-20 ">
-<table className="">
-  <thead>
-    <tr>
-      <th>Nome do livro</th>
-      <th>Autor</th>
-    </tr>
-  </thead>
-  <tbody>
-    {postss.map(function (val) {
+  //)
+    //  },[])
+      const [Nome,setNome]=useState("");
+      const [Senha,setSenha]=useState("");
+      
+      const data = {
+        Nome_aluno: Nome
+      };  
+      const enviar = () => {
+        axios.post('http://localhost/php/formAluno.php', JSON.stringify(data), {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(function (response) {
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            console.error('Erro na solicitação POST:', error);
+          });
+        
+      }
       return (
-        <tr key={val}>
+          <main className="h-screen w-screen">
+
+          <Nav/>
+
+          <input
+    type='text'
+    placeholder='Nome'
+    value={Nome}
+    onChange={(e) => setNome(e.target.value)}
+  className="bg-slate-500"
+  />
+
+  <button onClick={enviar}>Enviar</button>
+
+
+
+
+
+
+
+  <section className=" h-[91.5vh] w-full pt-20 ">
+  <table className="">
+    <thead>
+      <tr>
+        <th>Nome do livro</th>
+        <th>Autor</th>
+      </tr>
+    </thead>
+    <tbody>
+      {postss.map(function (val) {
+        return (
+          <tr key={val}>
           <td>{val.livro}</td>
-          <td>{val.Autor}</td>
-        </tr>
-      );
-    })}
-  </tbody>
-</table>    
-</section>
- 
-        </main>
+            <td>{val.Autor}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>    
+  </section>
+  
+          </main>
 
 
 
-    )
+      )
 
-}
+  }
