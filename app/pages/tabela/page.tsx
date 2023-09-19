739049c177c1397a8b910aa3d0cd2e0d0fa300da
Lima <joaovitorlima0411@gmail.com>
2023-09-19 00:13:51 -0300

    Select Atualizado

--- app/pages/tabela/page.tsx
+++ app/pages/tabela/page.tsx
@@ -5,6 +5,8 @@ import Nav from "../../componentes/nav/navb"
 import Aluno from "../../componentes/Tabelas/aluno"
 import Bibliotecario from "../../componentes/Tabelas/bibliotecario"
 import Livro from "../../componentes/Tabelas/livro"
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 
 export default function Tela(){
@@ -12,34 +14,24 @@ export default function Tela(){
 const [Tabelas,setTabelas]=useState(1)
 
     return(
-        <main className="h-screen w-screen">
+        <main className="h-full w-full">
 
 <Nav/>
 
-<select
-onChange={(e)=>setTabelas(e.target.value)}
- value={Tabelas}>
-    <option 
-    value={1}>
-        Aluno
-    
-    </option>
-
-    <option 
-    value={2}>
-        Bibliotecarios
-    </option>
-
-    
-    <option 
-    value={3}>
-        Livros
-    </option>
-
-    
-
-
-</select>
+<Select className="Selecao m-4 absolute right-80"
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        value={Tabelas}
+        label="Tabela"
+        onChange={(e)=>setTabelas(e.target.value)}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value={1}>Aluno</MenuItem>
+        <MenuItem value={2}>Bibliotecario</MenuItem>
+        <MenuItem value={3}>Livro</MenuItem>
+      </Select>
 
 {Tabelas==1 && <Aluno/>}
 {Tabelas==2 && <Bibliotecario/>}
